3d4bd5c5e4bf553963ac213e9127a2e029d23fc2
Shaiful Sujohn <shaiful@siliconorchard.com>
2018-12-07 09:40:48 +0600

    Initial Commit

---
+++ GenericTableVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GenericTableVC
+//
+//  Created by Md. Shaiful Islam on 12/7/18.
+//  Copyright © 2018 Md. Shaiful Islam Sujohn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
